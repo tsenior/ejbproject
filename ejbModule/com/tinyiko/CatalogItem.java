5d30571d838c70253f451fa8455f5305562f11e7
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-03-14 16:15:16 +0200

    make CatalogItem pojo class an Entity for JPA implementation - hibernate is a JPA producer

--- ejbModule/com/tinyiko/CatalogItem.java
+++ ejbModule/com/tinyiko/CatalogItem.java
@@ -2,20 +2,39 @@ package com.tinyiko;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="CATALOG_ITEM")
 public class CatalogItem {
 	
+	@Id
+	@Column(name = "CATALOG_ITEM_ID")
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long itemId;
+	
+	@Column(name = "NAME")
 	private String name;
+	
+	@Column(name = "MANUFACTURER")
 	private String manufacturer;
+	
+	@Column(name = "DESCRIPTION")
 	private String description;
+	
+	@Column(name = "AVAILABLE_DATE")
 	private LocalDate availableDate;
 	
 	public CatalogItem() {
 	}
 
-	public CatalogItem(Long itemId, String name, String manufacturer, String description, LocalDate availableDate) {
+	public CatalogItem(String name, String manufacturer, String description, LocalDate availableDate) {
 		super();
-		this.itemId = itemId;
 		this.name = name;
 		this.manufacturer = manufacturer;
 		this.description = description;
