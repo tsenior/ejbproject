64530e138e08a069beabfa11164b63a00ab01806
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-03-15 12:06:45 +0200

    added more methods and implementations

--- ejbModule/com/tinyiko/Catalog.java
+++ ejbModule/com/tinyiko/Catalog.java
@@ -19,7 +19,7 @@ public class Catalog implements CatalogLocal {
 	@PersistenceContext
 	private EntityManager entityManager;
 	
-	private List<CatalogItem> catalogItems = new ArrayList<>();
+	//private List<CatalogItem> catalogItems = new ArrayList<>();
 	
 	public Catalog() {
 		super();
@@ -33,6 +33,28 @@ public class Catalog implements CatalogLocal {
 	public void addItem(CatalogItem catalogItem) {
 		this.entityManager.persist(catalogItem);
 		
+	}
+
+	@Override
+	public CatalogItem findItem(Long catalogItemId) {
+		return this.entityManager.find(CatalogItem.class, catalogItemId);
+	}
+
+	@Override
+	public void deleteItem(CatalogItem catalogItem) {
+		//1. first check to see if it an item is within the persistence context
+		//2. if it is we just pass the item to delete
+		//3. if it is not we tell entity manager to perform merge which will add the item
+		this.entityManager.remove(this.entityManager.contains(catalogItem) ? catalogItem:this.entityManager.merge(catalogItem));
+		
+	}
+
+	@Override
+	public List<CatalogItem> searchByName(String name) {
+		
+		return this.entityManager.createQuery("select c from CatalogItem c " +
+				" where c.name like :name", CatalogItem.class).
+				setParameter("name", "%" + name + "%").getResultList();
 	}
 
 }
--- ejbModule/com/tinyiko/CatalogLocal.java
+++ ejbModule/com/tinyiko/CatalogLocal.java
@@ -4,11 +4,20 @@ import java.util.List;
 
 import javax.ejb.Local;
 
+
+// EJB Interface
+
 @Local
 public interface CatalogLocal {
 	
 	public List<CatalogItem> getItems();
 	
 	public void addItem(CatalogItem catalogItem);
+	
+	public CatalogItem findItem(Long catalogItemId);
+	
+	public void deleteItem(CatalogItem catalogItem);
+	
+	public List<CatalogItem> searchByName(String name);
 
 }
